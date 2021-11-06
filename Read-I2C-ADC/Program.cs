ded3ca336e935569b43943596cfb30cf8769c7cd
Adam Lyskawa <adam@codedog.pl>
2021-11-06 11:39:40 +0100

    FIX: Precission.

--- Read-I2C-ADC/Program.cs
+++ Read-I2C-ADC/Program.cs
@@ -4,7 +4,7 @@ using Iot.Device.Ads1115;
 
 using Read_I2C_ADC;
 
-var reader = new Reader(1, 0x48, MeasuringRange.FS6144, DataRate.SPS016);
+var reader = new Reader(1, 0x48, MeasuringRange.FS0256, DataRate.SPS016);
 while (true) {
-    Console.WriteLine($"Reading: RAW={reader.ADS.ReadRaw()}, Voltage={reader.ADS.ReadVoltage()}");
+    Console.WriteLine($"Reading: RAW={reader.ADS.ReadRaw()}, Voltage={reader.ADS.ReadVoltage().Millivolts}");
 }
