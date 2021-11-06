157c6ddd87ae0241c5c5ead487965df11d2dcace
Adam Lyskawa <adam@codedog.pl>
2021-11-06 10:53:09 +0100

    Add project files.

---
+++ Read-I2C-ADC/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+using Read_I2C_ADC;
+
+var reader = new Reader(0, 0x48);
+while (true) {
+    Console.WriteLine($"Reading: {reader.Read()}");
+}
---
+++ Read-I2C-ADC/Reader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Device.I2c;
+using System.Runtime.InteropServices;
+
+namespace Read_I2C_ADC;
+
+internal class Reader {
+
+    public int BusId { get; set; }
+    public int DeviceAddress { get; set; }
+
+    public Reader(int busId, int deviceAddress) {
+        BusId = busId;
+        DeviceAddress = deviceAddress;
+        Device = I2cDevice.Create(new I2cConnectionSettings(BusId, DeviceAddress));
+    }
+
+    public unsafe short Read() {
+        var buffer = new byte[2];
+        fixed (void* u_buffer = buffer) {
+            Device.Read(buffer.AsSpan());
+            var p_buf = new IntPtr(u_buffer);
+            return Marshal.ReadInt16(p_buf, 0);
+        }
+    }
+
+    private readonly I2cDevice Device;
+
+}
